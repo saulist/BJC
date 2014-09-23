0fee01c9b3df3bf96bdefd509e7d896b4ed11e7a
NicolasKAROLAK <nicolas@karolak.fr>
2014-09-23 21:01:17 +0200

    remplacement guillemets

---
+++ scripts/guillemets.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+sed -i 's/ "/ «~/g' ../tex/bjc_2014/*.tex
+sed -i 's/" /~» /g' ../tex/bjc_2014/*.tex
+sed -i 's/ ~"/ «~/g' ../tex/bjc_2014/*.tex
+sed -i 's/",/~»,/g' ../tex/bjc_2014/*.tex
+sed -i 's/"~/~»/g' ../tex/bjc_2014/*.tex
+sed -i 's/"\./~»\./g' ../tex/bjc_2014/*.tex
+sed -i 's/\["/\[«~/g' ../tex/bjc_2014/*.tex
+sed -i 's/"\]/~»\]/g' ../tex/bjc_2014/*.tex
+sed -i 's/ ~/ «~/g' ../tex/bjc_2014/*.tex
+sed -i 's/~ /~» /g' ../tex/bjc_2014/*.tex
+sed -i 's/~\./~»\./g' ../tex/bjc_2014/*.tex
+sed -i 's/~,/~»,/g' ../tex/bjc_2014/*.tex
+sed -i 's/« /«~/g' ../tex/bjc_2014/*.tex
+sed -i 's/ »/~»/g' ../tex/bjc_2014/*.tex
+
