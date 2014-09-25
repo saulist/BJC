64ecaf70462764ede0f176f9e4ae857f760c4d82
NicolasKAROLAK <nicolas@karolak.fr>
2014-09-25 20:08:53 +0200

    guillemets

--- scripts/guillemets.sh
+++ scripts/guillemets.sh
@@ -14,4 +14,5 @@ sed -i 's/~\./~»\./g' ../tex/bjc_2014/*.tex
 sed -i 's/~,/~»,/g' ../tex/bjc_2014/*.tex
 sed -i 's/« /«~/g' ../tex/bjc_2014/*.tex
 sed -i 's/ »/~»/g' ../tex/bjc_2014/*.tex
+sed -i 's/{~/{«~/g' ../tex/bjc_2014/*.tex
 
